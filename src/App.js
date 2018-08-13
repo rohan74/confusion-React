88a5102a7798a2f62fc22b386be4928a75564db3
Rohan <rohanro74@gmail.com>
2018-08-13 06:52:54 +0530

    adding card to components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { Menu } from './components/MenuComponents';
+import { DISHES } from './shared/dishes.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Con fusion</NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
