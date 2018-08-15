127152e4708193881be6d74460668b5e95420f04
Rohan <rohanro74@gmail.com>
2018-08-15 12:34:16 +0530

    resolved error in dishDetailComponent

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -17,7 +17,7 @@ export class DishDetail extends Component {
               year: 'numeric',
               month: 'short',
               day: '2-digit'
-            }).format(new Date(Date.parse(comment.date)))}
+            }).format(new Date(Date.parse(item.date)))}
           </p>
           <p>{item.comment}</p>
         </div>
@@ -33,29 +33,28 @@ export class DishDetail extends Component {
 
   render() {
     const { dish } = this.props;
-    console.log(dish);
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-5 col-12 m-1" key={this.props.dish.id}>
-            <Card key={this.props.dish.id}>
-              <CardImg
-                width="100%"
-                src={this.props.dish.image}
-                alt={this.props.dish.name}
-              />
-              <CardBody>
-                <CardTitle>{this.props.dish.name}</CardTitle>
-                <CardText>{this.props.dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div className="col-12 col-md-5 m-1" key={this.props.dish.id}>
-            <h4>Comments</h4>
-            {this.renderComments(this.props.dish.comments)}
+    if (dish != null) {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 col-12 m-1" key={dish.id}>
+              <Card key={dish.id}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+              <h4>Comments</h4>
+              {this.renderComments(dish.comments)}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return null;
+    }
   }
 }
